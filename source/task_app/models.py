8170cdb50e91cbfa4d43202df36486bbf5c84711
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-02-12 20:41:28 +0600

    Создал класс Task

--- source/task_app/models.py
+++ source/task_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    desc = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Описание")
+    status = models.TextField(max_length=200, null=False, blank=False, verbose_name="Статус")
+    author = models.CharField(max_length=200, null=False, blank=False, verbose_name="Автор")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+    updated_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время обновления")
+
+    def __str__(self):
+        return f"{self.author} - {self.desc}"
