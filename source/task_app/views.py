15d1a6e2aaefa6ec9c38ea661bafd631aaeef785
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-02-12 17:53:39 +0600

    Создал директорию views

--- source/task_app/views.py
+++ source/task_app/views.py
@@ -1,5 +1,6 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
 # Create your views here.
-def index_view(request):
+def index_view(request: WSGIRequest):
     return render(request, 'index.html')
