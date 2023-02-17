4f40597774c602d820682ad8a68b772239305fef
Kudaibergen Kyrgyzbay <kyrgyzbay.kudaibergen@gmail.com>
2023-02-17 12:11:52 +0600

    Добавил полное описание

--- source/task_app/views/tasks.py
+++ source/task_app/views/tasks.py
@@ -5,18 +5,21 @@ from django.urls import reverse
 
 from task_app.models import Task
 
+from task_app.models import StatusChoice
+
 
 def add_view(request: WSGIRequest):
     if request.method == "GET":
-        return render(request, 'task_create.html')
+        return render(request, 'task_create.html', context={'choices': StatusChoice.choices})
     task_data = {
-        'desc': request.POST.get('desc'),
+        'title': request.POST.get('title'),
+        'full_desc': request.POST.get('full_desc'),
         'status': request.POST.get('status'),
-        'date': request.POST.get('date'),
+        'custom_date': request.POST.get('custom_date'),
         'author': request.POST.get('author')
     }
     task = Task.objects.create(**task_data)
-    return redirect('detail_view', pk = task.pk)
+    return redirect('detail_view', pk=task.pk)
 
 
 def detail_view(request, pk):
@@ -24,7 +27,6 @@ def detail_view(request, pk):
     return render(request, 'task.html', context={
         'task': task
     })
-
 
 
 def remove_view(request, pk):
